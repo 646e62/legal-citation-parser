5465e76ef7ec177ad6a4b8a4f5e52839eb7da081
Daniel Booy <daniel@danielbooy.com>
2024-08-04 17:58:53 +0000

    Separated citation object creation and parsing

--- legal_citation_parser/citation_parser.py
+++ legal_citation_parser/citation_parser.py
@@ -2,6 +2,30 @@
 
 from .canlii_rules import CanLIICitationParser
 
+def create_citation(citation: str, citation_type: str = "canlii", **kwargs) -> dict:
+    """
+    Creates a citation object from a citation string.
+
+    Args:
+        citation (str): The citation string to parse.
+        type (str): The type of citation to parse. Default is "canlii".
+
+    Keyword Args:
+        include_url (bool): A flag to determine whether to include the CanLII URL in the output.
+        
+
+    Returns:
+        ParsedCitation: An object containing the parsed information, including the style of cause,
+        citation, citation type (neutral or CanLII), year, court code, decision number,
+        jurisdiction, court name, and court level.
+    """
+
+    parser = CanLIICitationParser(citation, **kwargs)
+    citation_info = parser.parse()
+    
+    return citation_info
+
+
 def parse_citation(
     citation: str, citation_type: str = "canlii", **kwargs
 ) -> dict:
@@ -22,7 +46,6 @@ def parse_citation(
         jurisdiction, court name, and court level.
     """
 
-    parser = CanLIICitationParser(citation, **kwargs)
-    citation_info = parser.parse()
+    citation_info = create_citation(citation, citation_type, **kwargs)
     
-    return citation_info
+    return citation_info.full()
